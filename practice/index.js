8fd46d89be89d957560d1a9c3f67c3ed0d359494
ritam-g <ritammaty2006@gmail.com>
2026-06-03 18:48:58 +0530

    docs: update 2 MD files
    - 2 files changed: 85 insertions(+), 11 deletions(-)

--- practice/index.js
+++ practice/index.js
@@ -59,11 +59,86 @@ async function call() {
 // const arr = [5, 2, 9, 1, 7];
 
 // console.log(arr.reduce((acc, ele) => acc < ele ? ele : acc, 0))
-const fruits = ["apple", "banana", "apple"];
+// const fruits = ["apple", "banana", "apple"];
 
-console.log(fruits.reduce((acc, frut) => {
-     acc[frut]= (acc[frut] || 0)+1
+// console.log(fruits.reduce((acc, frut) => {
+//      acc[frut]= (acc[frut] || 0)+1
 
-     return acc
+//      return acc
 
-},{}))
+// },{}))
+
+
+// const arr = ["a", "b", "a", "c", "b", "a"];
+
+// console.log(arr.reduce((acc, key) => {
+//     acc[key] = (acc[key] || 0) + 1
+//     return acc
+// }, {}))
+
+// const users = [
+//     { name: "Ritam", age: 20 },
+//     { name: "John", age: 20 },
+//     { name: "Alex", age: 25 }
+// ];
+
+// console.log(users.reduce((acc, { name, age }) => {
+//     if (acc[age]) {
+//         acc[age] = [...acc[age], { age, name }]
+//     }
+//     else {
+//         acc[age] = [{ age, name }]
+//     }
+//     return acc
+// }, {}))
+
+// {
+//     age[{}{}]
+
+// }
+
+const products = [
+    { name: "Laptop", price: 50000 },
+    { name: "Mouse", price: 1000 },
+    { name: "Phone", price: 30000 }
+];
+// {
+//   name: "Laptop",
+//   price: 50000
+// }
+
+// console.log(products.reduce((acc, { name, price }) => {
+//     if (acc < price) {
+//         acc = { name, rpice }
+//     }
+//     return acc
+// }, products[0]))
+
+const arr = [1, 2, 3, 4];
+
+// console.log(arr.reduce((acc, ele) => {
+//     acc.push(ele*2)
+//     return acc
+// },[]))
+// {
+//   mobile: ["iPhone", "Samsung"],
+//   laptop: ["MacBook"]
+// }
+
+
+// console.log([
+//     { category: "mobile", name: "iPhone" },
+//     { category: "laptop", name: "MacBook" },
+//     { category: "mobile", name: "Samsung" }
+// ].reduce((acc, { category, name }) => {
+//     if (acc[category]) {
+//         acc[category] = [...acc[category], name]
+//     }
+//     else {
+//         acc[category]=[name]
+//     }
+//     return acc
+// }, {}))
+
+// // console.log(arr.splice(0,2))
+// console.log(arr.includes(1))
