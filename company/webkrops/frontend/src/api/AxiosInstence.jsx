3bd8952272bc77ea0917d652bc1b48ba4df23d24
ritam-g <ritammaty2006@gmail.com>
2026-06-06 16:53:56 +0530

    feat(company): update AxiosInstence.jsx
    - 1 file changed: 21 insertions(+), 3 deletions(-)

--- company/webkrops/frontend/src/api/AxiosInstence.jsx
+++ company/webkrops/frontend/src/api/AxiosInstence.jsx
@@ -1,9 +1,27 @@
 import axios from 'axios'
 
 const AxiosInstence = axios.create({
-    baseURL:"",
-    withCredentials:true
+    baseURL: "",
+    withCredentials: true
 })
 
-// now for the send respose 
+//for the resoponse
 
+AxiosInstence.interceptors.response.use(
+    (res) => { return res },
+    (err) => {
+        // chke the statuse if 401 tehn aain call and then the previous reqcall
+        let originalRequest = err.config
+
+        if (err.response.status == 401 && !originalRequest._retray) {
+            originalRequest._retray = true
+            try {
+                await axios.post('http://localhost:3000/rotate')
+                return AxiosInstence(originalRequest)
+            } catch (error) {
+                return Promise.reject(error)
+            }
+        }
+    }
+
+)
